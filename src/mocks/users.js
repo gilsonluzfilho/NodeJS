1aa5be0233725a5bb8757bb4f2ebe97a706b24b4
gilsonluzfilho <gilsonfilho1994@yahoo.com.br>
2023-09-21 11:36:06 -0300

    usuarios

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [ 
+    {
+        id: 1,
+        name: 'Gilson',
+    },
+    {
+        id: 2,
+        name: 'Ana',
+    },
+    {
+        id: 3,
+        name: 'João das neves',
+    }
+];
